c53cc52698d216b7493849885c4fca3b18865b46
atikdev <atikdev.bdd@gamil.com>
2025-10-22 14:17:12 +0600

    navbar done with responsive

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">this is assignment 9</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import logo from "../../assets/Untitled_design-removebg-preview.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar  shadow-sm p-4 bg-pink-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+              <li>Logout</li>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          <img className="w-44" src={logo} alt="" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 space-x-2">
+          <li>Home</li>
+          <li>Profile</li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="w-10 rounded-full">
+          <img
+            className="rounded-full"
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
